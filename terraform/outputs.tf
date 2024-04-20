7a4361d3a7077f976ab81fb3e1a79dab208cb7ba
ovidiucs <ovidiu.ctin@gmail.com>
2024-04-20 18:17:31 +0300

    Switching to terragrunt

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aks_cluster_name" {
-  description = "The name of the AKS cluster"
-  value       = module.aks.aks_name
-}
-
-output "aks_cluster_id" {
-  description = "The ID of the AKS cluster"
-  value       = module.aks.aks_id
-}
